e8119ac16a2c9f9befd896879f778f943b3d47b2
Marc <mkramerdavis@my365.bellevue.edu>
2026-03-18 17:40:58 -0500

    Added assignment 1.2 KramerHelloWorld.java file.

---
+++ Module_1/HelloWorldApp/KramerHelloWorld.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        //Display message "Hello World from Marc Kramer!"//
+        System.out.println("Hello, World from Marc Kramer!");
+    }
+}
