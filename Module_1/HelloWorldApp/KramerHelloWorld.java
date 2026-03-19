8ff8cc828e312299208cc3bad7fc621623ffaabb
Marc <mkramerdavis@my365.bellevue.edu>
2026-03-18 19:07:32 -0500

    Updated assignment 1.2 KramerHelloWorld.java file.

--- Module_1/HelloWorldApp/KramerHelloWorld.java
+++ Module_1/HelloWorldApp/KramerHelloWorld.java
@@ -1,10 +1,10 @@
-/* Assignment 1.2
- * 
- * Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
- *     Comprehensive Version (12th ed.). Pearson Education, Inc.
- * Modified by M. Kramer-Davis 2026
- * 
- */
+/*  Assignment 1.2
+   
+    Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+        Comprehensive Version (12th ed.). Pearson Education, Inc.
+    Modified by M. Kramer-Davis 2026
+
+*/
 
 public class KramerHelloWorld {
     //main method to display "Hello World from Marc Kramer!"//
